1264dbb5f754696ab9fad79d64fa93166c493c76
JEIT25 <jeroldash.amora@gmail.com>
2024-10-28 19:14:36 +0800

    commit.

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    server: {
-        host: '192.168.1.3', //configure ip address evrytime network is change
-        port: 5173,
-        https: false,
-    },
+    // server: {
+    //     host: '192.168.1.38', //configure ip address evrytime network is change
+    //     port: 5173,
+    //     https: false,
+    // },
     plugins: [
         laravel(['resources/js/app.js']),
         vue({
