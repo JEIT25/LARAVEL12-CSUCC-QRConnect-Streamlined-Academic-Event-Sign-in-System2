4a791f49d92e7d6e5ed79787d121dfbcb26c991b
JEIT25 <jeroldash.amora@gmail.com>
2024-11-24 17:41:39 +0800

    added footer responsive.

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -146,7 +146,7 @@
             </header>
 
             <!-- Add your main content here -->
-            <main>
+            <main class="min-h-screen">
                 <!-- Success Message -->
                 <div class="fixed left-0 right-0 mb-4 border rounded-md shadow-md border-green-200 bg-green-100 p-2 text-center text-black-100 font-semibold"
                     v-if="successMess">
@@ -162,29 +162,28 @@
                 <!-- Slot for additional content -->
                 <slot></slot>
 
-                <footer class="bg-slate-950 text-yellow-300 p-6">
-                    <div
-                        class="container mx-auto flex flex-col md:flex-row justify-around items-center space-y-4 md:space-y-0">
-                        <!-- Logo and Title -->
-                        <div class="text-center md:text-left">
-                            <Link
-                                class="text-yellow-500 font-bold text-lg md:text-md flex items-center justify-center md:justify-start"
-                                href="/">
-                            <img :src="page.props.logoUrl" alt="Logo" width="30" height="24"
-                                class="mr-2 bg-white rounded-sm">
-                            CSUCC QRConnect
-                            </Link>
-                        </div>
-
-                        <!-- Copyright Text -->
-                        <p class="text-sm md:text-md text-center md:text-right">
-                            Copyright All Right Reserved &copy; 2024
-                        </p>
-
-                    </div>
-                </footer>
-
             </main>
+            <footer class="bg-slate-950 text-yellow-300 p-6 relative bottom-0 w-full">
+                <div
+                    class="container mx-auto flex flex-col md:flex-row justify-around items-center space-y-4 md:space-y-0">
+                    <!-- Logo and Title -->
+                    <div class="text-center md:text-left">
+                        <Link
+                            class="text-yellow-500 font-bold text-lg md:text-md flex items-center justify-center md:justify-start"
+                            href="/">
+                        <img :src="page.props.logoUrl" alt="Logo" width="30" height="24"
+                            class="mr-2 bg-white rounded-sm">
+                        CSUCC QRConnect
+                        </Link>
+                    </div>
+
+                    <!-- Copyright Text -->
+                    <p class="text-sm md:text-md text-center md:text-right">
+                        Copyright All Right Reserved &copy; 2024
+                    </p>
+
+                </div>
+            </footer>
         </div>
     </div>
 </template>
